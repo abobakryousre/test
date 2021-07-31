2ede0cfcc967df06106f6e06dec942ed26e7e9d4
Abobakr <abobakryousre@gmail.com>
2021-07-31 23:13:44 +0200

    feat(store-crud): update store endpoin

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -4,6 +4,8 @@ const statusCode = require("../helper/statusCode");
 
 const createStore = (req, res, next) => {
   const data = req.body;
+
+  // validate data before create a new store
   validator.validateCreateStore(data, async (err) => {
     if (err) return res.status(statusCode.BadRequest).json({ error: err });
 
@@ -15,24 +17,65 @@ const createStore = (req, res, next) => {
 
 const getAllStore = async (req, res, next) => {
   const allStores = await StoreModel.find({});
+
+  // if no document in the database
   if (allStores.length == 0) return res.status(statusCode.NoContent).end();
   return res.json(allStores);
 };
 
 const deleteStore = async (req, res, next) => {
   const storeId = req.params.id;
-  const storeExist = await StoreModel.exists({ _id: storeId });
-  if (storeExist) {
+  try {
+    const storeExist = await StoreModel.exists({ _id: storeId });
+    // if not exist return
+    if (!storeExist) {
+      return res
+        .status(statusCode.BadRequest)
+        .json({ errors: "store does not exist!" });
+    }
     await StoreModel.findByIdAndDelete({ _id: storeId });
     return res.status(statusCode.Success).end();
-  } else {
+  } catch (error) {
     return res
       .status(statusCode.BadRequest)
-      .json({ errors: "store does not exist!" });
+      .json({ errors: "store id not correct !" });
   }
 };
+
+const updateStore = async (req, res, next) => {
+  const storeId = req.params.id;
+  const data = req.body;
+
+  try {
+    const storeExist = await StoreModel.exists({ _id: storeId });
+
+    // if not exist return with error
+    if (!storeExist)
+      return res
+        .status(statusCode.BadRequest)
+        .json({ errors: "store does not exist!" });
+
+    // validate the updated body
+    validator.validateCreateStore(data, async (err) => {
+      if (err) return res.status(statusCode.BadRequest).json({ error: err });
+
+      const updatedStore = await StoreModel.findOneAndUpdate(
+        { _id: storeId },
+        { ...data },
+        { new: true }
+      );
+      return res.status(statusCode.Success).json(updatedStore);
+    });
+  } catch (error) {
+    return res
+      .status(statusCode.BadRequest)
+      .json({ errors: "store id not correct !" });
+  }
+};
+
 module.exports = {
   createStore,
   getAllStore,
   deleteStore,
+  updateStore,
 };
--- routes/storeRouter.js
+++ routes/storeRouter.js
@@ -1,10 +1,23 @@
 const express = require("express");
 const storeController = require("../controllers/storeController");
 const Router = express.Router();
+/**
+ * @url /stroe
+ * @methods get, post
+ */
 
 Router.route("/")
   .post(storeController.createStore)
   .get(storeController.getAllStore);
 
-Router.route("/:id").delete(storeController.deleteStore);
+/**
+ * @url /stroe/id
+ * @params storeId
+ * @methods delete, patch
+ */
+
+Router.route("/:id")
+  .delete(storeController.deleteStore)
+  .patch(storeController.updateStore);
+
 module.exports = Router;

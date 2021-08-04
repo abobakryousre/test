02ca40b3c32f004337cc6efb33a2f92b7d723d57
Abobakr <abobakryousre@gmail.com>
2021-08-04 03:41:48 +0200

    deploy v3

--- index.js
+++ index.js
@@ -6,30 +6,25 @@ const cors = require("cors");
 
 // get the enviornment configs
 const ENV = process.env.NODE_ENV || "dev";
-const { DB_URI, PORT } = configs[ENV];
+const { DB_URI, PORT } = configs["dev"];
 
 // connect to data base
-// mongoose
-//   .connect(DB_URI, {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//     useFindAndModify: false,
-//     useCreateIndex: true,
-//   })
-//   .then(() => {
-// app.listen(process.env.PORT || PORT, (err) => {
-//   if (err) return console.log(err.message);
-//   console.log("Server started on port: " + process.env.PORT || PORT);
-// });
-//   })
-//   .catch((err) => {
-//     console.log(err.message);
-//   });
-
-app.listen(process.env.PORT || PORT, (err) => {
-  if (err) return console.log(err.message);
-  console.log("Server started on port: " + process.env.PORT || PORT);
-});
+mongoose
+  .connect(DB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  })
+  .then(() => {
+    app.listen(process.env.PORT || PORT, (err) => {
+      if (err) return console.log(err.message);
+      console.log("Server started on port: " + process.env.PORT || PORT);
+    });
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
 
 app.use("/", (req, res) => {
   return res.json({ message: "hello montes" });

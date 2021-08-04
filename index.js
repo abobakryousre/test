ed3bf2d231fd1cd01a625f2c46e5ba0d34a7a1b1
Abobakr <abobakryousre@gmail.com>
2021-08-04 03:44:50 +0200

    deploy v4

--- index.js
+++ index.js
@@ -9,23 +9,27 @@ const ENV = process.env.NODE_ENV || "dev";
 const { DB_URI, PORT } = configs["dev"];
 
 // connect to data base
-mongoose
-  .connect(DB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false,
-    useCreateIndex: true,
-  })
-  .then(() => {
-    app.listen(process.env.PORT || PORT, (err) => {
-      if (err) return console.log(err.message);
-      console.log("Server started on port: " + process.env.PORT || PORT);
-    });
-  })
-  .catch((err) => {
-    console.log(err.message);
-  });
+// mongoose
+//   .connect(DB_URI, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//     useFindAndModify: false,
+//     useCreateIndex: true,
+//   })
+//   .then(() => {
+//     app.listen(process.env.PORT || PORT, (err) => {
+//       if (err) return console.log(err.message);
+//       console.log("Server started on port: " + process.env.PORT || PORT);
+//     });
+//   })
+//   .catch((err) => {
+//     console.log(err.message);
+//   });
 
+app.listen(process.env.PORT || PORT, (err) => {
+  if (err) return console.log(err.message);
+  console.log("Server started on port: " + process.env.PORT || PORT);
+});
 app.use("/", (req, res) => {
   return res.json({ message: "hello montes" });
 });

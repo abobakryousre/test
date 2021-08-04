f8b81c17b0ca9e0c79e06d931cc44ba8da62035a
Abobakr <abobakryousre@gmail.com>
2021-08-04 04:38:11 +0200

    deploy front end

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const path = require("path");
+app.use(express.json());
+app.use(cors());
+
+app.use(express.static(__dirname + "/dist/fron-end"));
+app.get("/*", function (req, res) {
+  res.sendFile(path.join(__dirname + "/dist/fron-end/index.html"));
+});
+
+app.listen(process.env.PORT || 8080, (err) => {
+  if (!err)
+    return console.log(`server runging on port ${process.env.PORT || 8080}`);
+  console.log(err);
+});
